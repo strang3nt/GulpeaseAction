b0df13daf91c0ddc15d6d407ad50db8c34a584eb
strang3nt <flori995@live.com>
2021-01-03 22:56:33 +0100

    Rimosse linee commentate

--- gulpease.py
+++ gulpease.py
@@ -13,25 +13,12 @@ lettere = len(re.findall(r'\w', testo)) + len(re.findall(r'\W', testo))
 punti = len(re.findall('[.]+\s', testo)) + len(re.findall('[;]+\s', testo)) - len(re.findall('[.]+\s+[.]', testo))
 
 indiceG=89+((300*punti)-(10*lettere))/parole
-# print("numero di parole presenti nei doc :   " + str(parole))
-# print("numero di lettere presenti nei doc :  " + str(lettere))
-# print("numero di frasi presenti nei doc :    " + str(punti))
-# print("")
+
 if parole!=0:
     if indiceG>100:
         indiceG=100
 
-    # print("indice di Gulpease restrittivo : " + str(indiceG))
-    print(str(indiceG))
-    punti = len(re.findall('[.]', testo))
-    indiceG = 89 + ((300 * punti) - (10 * lettere)) / parole
-    if indiceG>100:
-        indiceG=100
+    print(str(int(round(indiceG))))
 
-    # print("indice di Gulpease non restrittivo : " + str(indiceG))
-    # print("")
-    # print("Nel primo indice non viene considerato delimitatore di frase:")
-    # print("- la punteggiatura spazio punteggiatura come delimitatore di frasi")
-    # print("- la punteggiatura che non e' seguita da un carattere di spaziatura")
 else:
     print("Errore nel calcolo dell'indice Gulpease")
