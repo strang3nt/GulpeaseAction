11240d47d1fcfb6749cc99a1f94ec20562b09a46
Alessandro Flori <flori995@live.com>
2021-01-11 22:09:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /github/workspace
 mkdir Gulpease
+cd Gulpease
 wget studio-di-fattibilita.pdf https://github.com/CodeOfDutyJS/documentazione/releases/download/wip%2Fstudio-di-fattibilita/studio-di-fattibilita.pdf 
 wget norme-di-progetto.pdf https://github.com/CodeOfDutyJS/documentazione/releases/download/wip%2Fnorme-di-progetto/norme-di-progetto.pdf
 wget analisi-dei-requisiti.pdf https://github.com/CodeOfDutyJS/documentazione/releases/download/wip%2Fanalisi-dei-requisiti/analisi-dei-requisiti.pdf
@@ -10,11 +11,10 @@ ls *.pdf > Gulpease/pdfList.txt
 while IFS= read -r document || [ -n "$document" ]
 do
     pdftotext $document
-    python /gulpease.py ${document%.*}.txt > Gulpease/${document%.*}-eval.txt
+    python ../gulpease.py ${document%.*}.txt > Gulpease/${document%.*}-eval.txt
     cat Gulpease/${document%.*}-eval.txt
-    sed -e 's/<!-- GULP_'"${document%.*}"' -->.*<!-- end -->/<!-- GULP_'"${document%.*}"' -->'"$(cat Gulpease/${document%.*}-eval.txt)"' al '"$(date +'%Y-%m-%d')"'<!-- end -->/' -i README.md
-done < "Gulpease/pdfList.txt"
+    sed -e 's/<!-- GULP_'"${document%.*}"' -->.*<!-- end -->/<!-- GULP_'"${document%.*}"' -->'"$(cat Gulpease/${document%.*}-eval.txt)"' al '"$(date +'%Y-%m-%d')"'<!-- end -->/' -i ../README.md
+done < "pdfList.txt"
 tree .
-rm *.txt
-rm *.pdf
+cd ..
 rm -r Gulpease
