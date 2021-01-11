722dbbe9e3d99cc096394644608f1b58735d8556
Alessandro Flori <flori995@live.com>
2021-01-11 22:20:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ ls *.pdf > pdfList.txt
 while IFS= read -r document || [ -n "$document" ]
 do
     pdftotext $document
-    python ../gulpease.py ${document%.*}.txt > ${document%.*}-eval.txt
-    cat Gulpease/${document%.*}-eval.txt
+    python ../../gulpease.py ${document%.*}.txt > ${document%.*}-eval.txt
+    cat ${document%.*}-eval.txt
     sed -e 's/<!-- GULP_'"${document%.*}"' -->.*<!-- end -->/<!-- GULP_'"${document%.*}"' -->'"$(cat Gulpease/${document%.*}-eval.txt)"' al '"$(date +'%Y-%m-%d')"'<!-- end -->/' -i ../README.md
 done < "pdfList.txt"
 tree .
